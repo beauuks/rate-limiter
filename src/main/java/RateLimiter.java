e24ae0e6dd0a81c8c061c3ff5c8ccdf83ee5515b
Beau Sang <kanthadsa@gmail.com>
2025-12-31 21:54:55 +0700

    implemented fixed window

--- src/main/java/RateLimiter.java
+++ src/main/java/RateLimiter.java
@@ -1,20 +1,41 @@
 import redis.clients.jedis.Jedis;
 
 public class RateLimiter {
+    public static boolean allowRequest(Jedis jedis, String userId, int limit) {
+        // Hint: Use jedis.incr(userId)
+        long count = jedis.incr(userId);
+        System.out.println("Redis Count for " + userId + ": " + count);
+
+        if (count == 1) {
+            jedis.expire(userId, 60);
+        } 
+        
+        if (count > limit) {
+            return false;
+        } 
+        
+        return true;
+    }
+
     public static void main(String[] args) {
         try (Jedis jedis = new Jedis("redis", 6379)) {
+            jedis.del("hacker_1"); 
+            System.out.println("Hacker Attack Simulation");
 
-            jedis.set("visitor_count", "0");
-            System.out.println("Set visitor_count to 0");
-
-            long count = jedis.incr("visitor_count");
-            System.out.println("Increment visitor_count. Value: " + count);
-
-            String value = jedis.get("visitor_count");
-            System.out.println("Get value from visitor_count: " + value);
+            for (int i = 1; i <= 10; i++) {
+                boolean allowed = allowRequest(jedis, "hacker_1", 5);
+                
+                if (allowed) {
+                    System.out.println("Request " + i + ": ALLOWED");
+                } else {
+                    System.out.println("Request " + i + ": BLOCKED (Too Many Requests)");
+                }
+                
+                Thread.sleep(200); 
+            }
 
         } catch (Exception e) {
-            System.err.println("Could not connect to Redis: " + e);
+            e.printStackTrace();
         }
     }
 }
